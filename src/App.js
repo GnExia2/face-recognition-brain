34e2692eab672ed5e2980d3db0b4722083b70f7a
Garrick Ngai <ngai.garrick.j@gmail.com>
2019-06-25 12:03:37 -0700

    created Logo, Navigation, and ImageLinkSearchcomponents

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation.js'
+import Logo from './components/Logo/Logo.js'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navigation />
+        <Logo/>
+        <ImageLinkForm/>
       </div>
     );
   }
