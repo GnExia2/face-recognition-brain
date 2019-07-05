cab0cd951407a2e923743e0de616acac4e717321
Garrick Ngai <ngai.garrick.j@gmail.com>
2019-07-05 14:16:21 -0700

    installed bcrypt for encrypting passwords, and updated register post route

--- face-brain-api/server.js
+++ face-brain-api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs')
 
 
 const app = express();
@@ -42,6 +43,9 @@ app.post('/signin', (req,res) => {
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
+  bcrypt.hash(password, null, null, function( err, hash){
+    console.log(hash);
+  })
   database.users.push({
     id: '125',
     name: name,
